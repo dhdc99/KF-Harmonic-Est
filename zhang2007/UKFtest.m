6dfdcd6a6708c0f7dae998b94ebcc28170403722
dhdc99 <38541000+dhdc99@users.noreply.github.com>
2021-06-14 18:19:30 -0400

    Update UKFtest.m

--- zhang2007/UKFtest.m
+++ zhang2007/UKFtest.m
@@ -2,6 +2,11 @@ clc
 clear variables
 close all
 
+% TODO modify the signal model, REMOVE THE FREQUENCY STATE VARIABLES so the
+% covariance matrices and prior estimates do not get screwed. After
+% modification there should be 3 state variables for each harmonic: cosine,
+% sine, and amplitude.
+
 load('unknownSignal.mat');
 harmnum=10;
 [output,p] = ukfsample(signal,60,1/4000,harmnum,0.1,0,2,1,1);
