7126e570c84fe40840d3019189368f8460fcb6b3
dhdc99 <38541000+dhdc99@users.noreply.github.com>
2021-07-09 12:21:49 -0400

    Update entropytest.m

--- girgis1991/entropytest.m
+++ girgis1991/entropytest.m
@@ -11,4 +11,27 @@ dur= seconds(t);
 xt = timetable(dur',isum(1,:)',isum(2,:)',isum(3,:)',isum(4,:)');
 xt2 = timetable(dur',HIFSig(:,5),HIFSig(:,6),HIFSig(:,7),HIFSig(:,5)+HIFSig(:,6)+HIFSig(:,7));
 
-pentropy(xt2(:,4));
+embeddim = 2;
+
+normalentropy1 = sampen(isum(1,1000:4000),embeddim,0.1,'euclidean')
+hifentropy1 = sampen(isum(1,4000:16000),embeddim,0.1,'euclidean')
+
+normalentropy3 = sampen(isum(2,1000:4000),embeddim,0.1,'euclidean')
+hifentropy3 = sampen(isum(2,4000:16000),embeddim,0.1,'euclidean')
+
+normalentropy5 = sampen(isum(3,1000:4000),embeddim,0.1,'euclidean')
+hifentropy5 = sampen(isum(3,4000:16000),embeddim,0.1,'euclidean')
+
+normalentropy7 = sampen(isum(4,1000:4000),embeddim,0.1,'euclidean')
+hifentropy7 = sampen(isum(4,4000:16000),embeddim,0.1,'euclidean')
+
+figure
+tiledlayout('flow');
+nexttile
+pentropy(isum(1,:),4000);
+nexttile
+pentropy(isum(2,:),4000);
+nexttile
+pentropy(isum(3,:),4000);
+nexttile
+pentropy(isum(4,:),4000);
